ebe3fe08065cce1f5f95c08867708af9040dc041
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-11-08 23:11:35 +0100

    Tests for advanced tasks

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,1 +1,19 @@
 #include "sort.h"
+
+/**
+ * merge_sort - Implementing the merge sort algorithm
+ * @array: Array to sort
+ * @size: Size of array
+ */
+void merge_sort(int *array, size_t size)
+{
+	/* Template for Solution */
+	int x, y;
+
+	if (array == NULL || size < 2)
+		return;
+	x = array[0] + 1;
+	y = size - 1;
+	if (x + y > 5)
+		print_array(array, size);
+}
