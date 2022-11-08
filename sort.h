1085fb84fff372d643136adc6f93e9275847fe7f
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-11-08 22:49:00 +0100

    Updated sort.h

--- sort.h
+++ sort.h
@@ -27,7 +27,16 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
 
+/* Advanced Tasks */
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 
 /* Helper files definition*/
 void print_array(const int *array, size_t size);
