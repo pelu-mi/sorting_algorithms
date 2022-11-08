200d0154eadba151c210ac6f02176db6b33382d4
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-11-08 22:51:23 +0100

    Test for task 4

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,1 +1,11 @@
 #include "sort.h"
+
+/**
+ * shell_sort - Implementing the shell sort algorithm using Knuth sequence
+ * @array: Array to sort
+ * @size: Size of array
+ */
+void shell_sort(int *array, size_t size)
+{
+	return;
+}
